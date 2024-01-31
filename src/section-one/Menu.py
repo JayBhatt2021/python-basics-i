1ccf8bb837dfba1855f7d03c23fbe39967df5d64
Jay Bhatt <jay.bhatt98@gmail.com>
2024-01-31 15:07:52 -0500

    Refactored Menu.py

--- src/section-one/Menu.py
+++ src/section-one/Menu.py
@@ -1,5 +1,5 @@
 def main() -> None:
-    """Calculate sales tax and total price based on entered prices.
+    """Calculate the sales tax and total price based on the entered prices.
 
     :return: None
     """
@@ -7,16 +7,14 @@ def main() -> None:
         burger_price = float(input("Enter the burger price: "))
         fries_price = float(input("Enter the fries price: "))
         drink_price = float(input("Enter the drink price: "))
-        tax_rate = 0.1
 
-        # Calculate sales tax and total price
-        sales_tax = tax_rate * (burger_price + fries_price + drink_price)
+        # Calculates the sales tax and total price
+        sales_tax = 0.1 * (burger_price + fries_price + drink_price)
         total_price = burger_price + fries_price + drink_price + sales_tax
 
-        # Display results with proper formatting
-        print(f"Sales tax: ${sales_tax:.2f}")
+        # Displays the results with proper formatting
+        print(f"\nSales tax: ${sales_tax:.2f}")
         print(f"Total price: ${total_price:.2f}")
-
     except ValueError:
         print("Prices must be valid floats! Exiting program...")
 
