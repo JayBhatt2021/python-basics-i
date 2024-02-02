9027be2fceb6b7ad2f8250ace77bb137de173bbd
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-02 14:08:20 -0500

    Created SandClock.py

---
+++ src/section4/SandClock.py
@@ -0,0 +1,35 @@
+def print_sandclock(rows: int) -> None:
+    """Print a sandclock pattern with the specified number of rows.
+
+    :param rows: The number of rows for the upper-half the sandclock.
+    :return: None
+    """
+    # Prints the upper half of the sandclock
+    for i in range(rows):
+        spaces = " " * i
+        asterisks = "*" * (2 * (rows - i) - 1)
+        print(spaces + asterisks + spaces)
+
+    # Prints the lower half of the sandclock
+    for i in range(rows - 2, -1, -1):
+        spaces = " " * i
+        asterisks = "*" * (2 * (rows - i) - 1)
+        print(spaces + asterisks + spaces)
+
+
+def main() -> None:
+    """Print a sandclock pattern.
+
+    :return: None
+    """
+    try:
+        rows = int(input("Enter the number of rows for the sandclock: "))
+        print()
+        print_sandclock(rows)
+        print()
+    except ValueError:
+        print("The number of rows must be an integer! Exiting program...")
+
+
+if __name__ == "__main__":
+    main()
