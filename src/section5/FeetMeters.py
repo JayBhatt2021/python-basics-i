f0873d44e57c1086f1611b8b56ca059282b859bd
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-03 10:51:27 -0500

    Tweaked print_conversion_table() in FeetMeters.py

--- src/section5/FeetMeters.py
+++ src/section5/FeetMeters.py
@@ -1,3 +1,6 @@
+from typing import Callable, List
+
+
 def feet_to_meters(feet: float) -> float:
     """Convert feet to meters.
 
@@ -16,7 +19,10 @@ def meters_to_feet(meters: float) -> float:
     return 3.279 * meters
 
 
-def print_conversion_table(column_titles, conversion_function) -> None:
+def print_conversion_table(
+        column_titles: List[str],
+        conversion_function: Callable[[float], float]
+) -> None:
     """Print a conversion table.
 
     :param column_titles: A list of column titles.
@@ -26,8 +32,7 @@ def print_conversion_table(column_titles, conversion_function) -> None:
     print(f"{column_titles[0]}\t{column_titles[1]}")
 
     for i in range(0, 21):
-        print(f"{i:.1f}\t", end="")
-        print(f"{conversion_function(i):.3f}")
+        print(f"{i:.1f}\t{conversion_function(i):.3f}")
 
     print()
 
