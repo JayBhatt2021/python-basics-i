432ee1ff842604bd035253a21027f8619902f835
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-03 22:06:41 -0500

    Created Addition.py

---
+++ src/section1/Addition.py
@@ -0,0 +1,11 @@
+def main() -> None:
+    """Print the result of concatenating and adding 12 and 34.
+
+    :return: None
+    """
+    print(f"12 and 34 concatenated: {12}{34}")
+    print(f"12 and 34 added: {12 + 34}")
+
+
+if __name__ == "__main__":
+    main()
