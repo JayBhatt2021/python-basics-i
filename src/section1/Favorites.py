563017d345c1f5774a19dd68bb362bea5c6d40a6
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-04 18:29:30 -0500

    Created Favorites.py

---
+++ src/section1/Favorites.py
@@ -0,0 +1,14 @@
+def main() -> None:
+    """Display information about Dr. Martin Luther King Jr.
+
+    :return: None
+    """
+    print("Name: Dr. Martin Luther King Jr.")
+    print("Birthday: 01/15/1929")
+    print("Favorite hobbies: reading, writing, and singing")
+    print("Favorite book: Civil Disobedience by Henry David Thoreau")
+    print("Favorite food: macaroni and cheese")
+
+
+if __name__ == "__main__":
+    main()
