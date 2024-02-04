84e0bc641ad6c9b1dfa98c6e1fa5f8fcc7aaab43
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-03 22:20:17 -0500

    Created Echo.py

---
+++ src/section1/Echo.py
@@ -0,0 +1,11 @@
+def main() -> None:
+    """Simulate a simple user input program.
+
+    :return: None
+    """
+    user_input = input("Please enter something: ")
+    print(f'\nYou entered: "{user_input}".')
+
+
+if __name__ == "__main__":
+    main()
