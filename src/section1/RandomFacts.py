1672c6366a770e58e60f7780930bb72c15dfbcb4
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-03 22:26:26 -0500

    Created RandomFacts.py

---
+++ src/section1/RandomFacts.py
@@ -0,0 +1,14 @@
+def main() -> None:
+    """Share some random facts with the user.
+
+    :return: None
+    """
+    print("Here are some random facts:")
+    print("Australia is wider than the moon.")
+    print("Competitive art used to be an Olympic sport.")
+    print("Baby rabbits are called kits.")
+    print("The Spanish national anthem has no words.")
+
+
+if __name__ == "__main__":
+    main()
