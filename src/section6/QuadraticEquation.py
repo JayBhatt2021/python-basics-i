fb6bbc266d24e6d19857281327582ac6d1925068
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-03 20:07:09 -0500

    Refactored QuadraticEquation.py

--- src/section6/QuadraticEquation.py
+++ src/section6/QuadraticEquation.py
@@ -1,45 +1,41 @@
 import math
+from typing import Optional, Tuple
 
 
 class QuadraticEquation:
-    """"""
+    """Represents a quadratic equation of the form ax^2 + bx + c = 0."""
 
-    def __init__(self, a: float, b, c) -> None:
-        """Check if a number is a palindrome.
+    def __init__(self, a: float, b: float, c: float) -> None:
+        """Initialize the quadratic equation with coefficients.
 
-        :param num: The number to check.
-        :return: True if the number is a palindrome, False otherwise.
+        :param a: Coefficient of the quadratic term (ax^2).
+        :param b: Coefficient of the linear term (bx).
+        :param c: Constant term in the quadratic equation.
+        :return: None
         """
         self.a = a
         self.b = b
+        self.c = c
         self.discriminant = b ** 2 - 4 * a * c
 
-    def calculate_first_root(self):
-        """Check if a number is a palindrome.
+    def calculate_roots(self) -> Tuple[Optional[float], Optional[float]]:
+        """Calculate the roots of the quadratic equation.
 
-        :param num: The number to check.
-        :return: True if the number is a palindrome, False otherwise.
+        :return: A tuple containing the roots of the quadratic equation.
         """
         if self.discriminant < 0:
-            return None
-
-        return (-self.b + math.sqrt(self.discriminant)) / 2 * self.a
-
-    def calculate_second_root(self):
-        """Check if a number is a palindrome.
-
-        :param num: The number to check.
-        :return: True if the number is a palindrome, False otherwise.
-        """
-        if self.discriminant <= 0:
-            return None
-
-        return (-self.b - math.sqrt(self.discriminant)) / 2 * self.a
+            return None, None
+        elif self.discriminant == 0:
+            root = -self.b / (2 * self.a)
+            return root, None
+        else:
+            root1 = (-self.b + math.sqrt(self.discriminant)) / (2 * self.a)
+            root2 = (-self.b - math.sqrt(self.discriminant)) / (2 * self.a)
+            return root1, root2
 
 
 def main() -> None:
-    """Calculate the relationship between two circles based on their centers
-    and radii.
+    """Calculate the roots of a quadratic equation.
 
     :return: None
     """
@@ -49,8 +45,7 @@ def main() -> None:
         c = float(input("Enter c: "))
         quadratic_equation = QuadraticEquation(a, b, c)
 
-        root_one = quadratic_equation.calculate_first_root()
-        root_two = quadratic_equation.calculate_second_root()
+        root_one, root_two = quadratic_equation.calculate_roots()
 
         print(f"\nRoot #1: {root_one if root_one is not None else 'Undefined'}")
         print(f"Root #2: {root_two if root_two is not None else 'Undefined'}")
