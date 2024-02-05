e2389726d0ce6a2ce20542e4f1f77c5b1e9059e7
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-05 18:19:47 -0500

    Tweaked Occurrences.py

--- src/section7/Occurrences.py
+++ src/section7/Occurrences.py
@@ -2,42 +2,42 @@ from typing import List
 
 
 def count(num_list: List[int]) -> None:
-    """Print a sandclock pattern with the specified number of rows.
+    """Print the count of occurrences for each number in the given list.
 
-    :param rows: The number of rows for the upper-half of the sandclock.
+    :param num_list: A list of integers.
     :return: None
     """
     num_frequency = {}
 
+    # Counts the occurrences of each number
     for num in num_list:
-        if num in num_frequency:
-            num_frequency[num] += 1
-        else:
-            num_frequency[num] = 1
+        num_frequency[num] = num_frequency.get(num, 0) + 1
 
-    print("\nCount:")
+    # Displays the count for each number
+    print("\nCounts:")
     for num, frequency in num_frequency.items():
-        if frequency == 1:
-            print(f"{num} occurred 1 time.")
-        else:
-            print(f"{num} occurred {frequency} times.")
+        occurrence_str = "time" if frequency == 1 else "times"
+        print(f"{num} occurred {frequency} {occurrence_str}.")
 
 
 def main() -> None:
-    """Print a sandclock pattern.
+    """Get user input for a list of numbers and display the count of
+    occurrences.
 
     :return: None
     """
     try:
         num_list = []
 
+        # Get users input for a list of numbers
         for i in range(10):
             num = int(input(f"Enter Number #{i + 1}: "))
             num_list.append(num)
 
+        # Displays the count of occurrences
         count(num_list)
     except ValueError:
-        print("The number of rows must be an integer! Exiting program...")
+        print("Invalid input! Please enter an integer. Exiting program...")
 
 
 if __name__ == "__main__":
