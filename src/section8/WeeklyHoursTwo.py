fce030b092b75192267d932df608c6e26bcea1e5
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-06 13:56:43 -0500

    Refactored WeeklyHoursTwo.py

--- src/section8/WeeklyHoursTwo.py
+++ src/section8/WeeklyHoursTwo.py
@@ -1,12 +1,18 @@
 from typing import List, Tuple
 import random
 
+NUM_EMPLOYEES = 3
+"""The number of employees."""
+
+NUM_DAYS = 7
+"""The number of working days."""
+
 DAYS_OF_WEEK = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 """A list containing the days of the week."""
 
 
 def generate_random_work_hours(
-    num_employees: int = 3, num_days: int = 7
+    num_employees: int = NUM_EMPLOYEES, num_days: int = NUM_DAYS
 ) -> List[List[int]]:
     """Generate a list of random work hours for each employee for each day of
     the week.
@@ -30,11 +36,11 @@ def display_work_hours_table(work_hours: List[List[int]]) -> None:
     print("Employee Work Hours:")
     print("\t" + "\t".join(DAYS_OF_WEEK))
 
-    for employee_num, week_hours in enumerate(work_hours, start=1):
+    for emp_num, week_hours in enumerate(work_hours, start=1):
         formatted_hours = [
             # Constructs the string representing the day's work hours along with
             # the employee number if it is the first day of the week
-            f"E{employee_num}\t{day_hours}" if i == 0 else str(day_hours)
+            f"E{emp_num}\t{day_hours}" if i == 0 else str(day_hours)
             for i, day_hours in enumerate(week_hours)
         ]
 
@@ -43,27 +49,23 @@ def display_work_hours_table(work_hours: List[List[int]]) -> None:
 
 
 def display_most_hours_worked(work_hours: List[List[int]]) -> None:
-    """Display the work hours in a formatted table.
+    """Display the day with the most worked hours for each employee.
 
     :param work_hours: A list of work hours for each employee for each day.
     :return: None
     """
     print()
+    for emp_num, week_hours in enumerate(work_hours, start=1):
+        _, max_day = max(
+            # Pairs each day's work hours with the corresponding day of the week
+            zip(week_hours, DAYS_OF_WEEK),
 
-    for employee_num, week_hours in enumerate(work_hours, start=1):
-        max_hours = week_hours[0]
-        max_hours_day = DAYS_OF_WEEK[0]
-
-        #
-        for i in range(len(week_hours)):
-            if week_hours[i] > max_hours:
-                max_hours = week_hours[i]
-                max_hours_day = DAYS_OF_WEEK[i]
-
-        print(
-            f"Employee #{employee_num} worked the most hours on "
-            f"{max_hours_day}."
+            # Selects the maximum value based on the first element of each pair
+            # (daily work hours)
+            key=lambda x: x[0]
         )
+
+        print(f"Employee #{emp_num} worked the most hours on {max_day}.")
 
 
 def calculate_weekly_hours(work_hours: List[List[int]]) \
@@ -75,8 +77,8 @@ def calculate_weekly_hours(work_hours: List[List[int]]) \
     """
     # Calculates the weekly work hours for each employee
     weekly_hours = [
-        (employee_num, sum(week_hours))
-        for employee_num, week_hours in enumerate(work_hours, start=1)
+        (emp_num, sum(week_hours))
+        for emp_num, week_hours in enumerate(work_hours, start=1)
     ]
 
     # Sorts the weekly_hours list based on the total hours worked in a week
@@ -94,12 +96,12 @@ def display_weekly_hours(weekly_hours: List[Tuple[int, int]]) -> None:
     """
     print("\nEmployee #\tWeekly Hours")
     print("-----------------------------")
-    for employee_num, week_hours in weekly_hours:
+    for emp_num, week_hours in weekly_hours:
         # Prints leading spaces
         print(" " * 5, end="")
 
         # Prints employee number
-        print(employee_num, end="")
+        print(emp_num, end="")
 
         # Prints in-between spaces
         print(" " * 15, end="")
@@ -109,20 +111,21 @@ def display_weekly_hours(weekly_hours: List[Tuple[int, int]]) -> None:
 
 
 def main() -> None:
-    """Generate and display random work hours, along with additional statistics.
+    """Generate random work hours for employees, display them in a tabular
+    format, and show additional statistics.
 
     :return: None
     """
-    # Generates random work hours
+    # Generates random work hours for employees
     work_hours = generate_random_work_hours()
 
-    # Displays the work hours in a 3x7 table
+    # Displays the work hours in a tabular format
     display_work_hours_table(work_hours)
 
-    #
+    # Displays which day each employee worked the most hours
     display_most_hours_worked(work_hours)
 
-    #
+    # Calculates and displays the total weekly work hours for each employee
     weekly_hours = calculate_weekly_hours(work_hours)
     display_weekly_hours(weekly_hours)
 
