968750936a9b2e3ecc2a5fd8c5a51ea221dec9a3
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-07 18:22:35 -0500

    Refactored CompareLists.py

--- src/section7/CompareLists.py
+++ src/section7/CompareLists.py
@@ -1,11 +1,11 @@
 def get_list_size() -> int:
-    """Prompt the user for the class size.
+    """Prompt the user for the size of the lists.
 
-    :return: The class size entered by the user.
+    :return: The size of the lists entered by the user.
     """
     while True:
         try:
-            list_size = int(input("Enter the list size: "))
+            list_size = int(input("Enter the size of the lists: "))
             if list_size > 0:
                 return list_size
             else:
@@ -15,14 +15,14 @@ def get_list_size() -> int:
 
 
 def get_integers(list_size: int) -> list:
-    """Prompt the user to enter scores for each student.
+    """Prompt the user to enter integers for each list.
 
-    :param class_size: The number of students in the class.
-    :return: A list containing the scores entered by the user.
+    :param list_size: The size of the lists.
+    :return: A list containing the integers entered by the user.
     """
     integers = []
 
-    print(f"Enter the {list_size} integer(s):")
+    print(f"Enter the {list_size} integer(s) for the list:")
     for i in range(1, list_size + 1):
         while True:
             try:
@@ -36,16 +36,20 @@ def get_integers(list_size: int) -> list:
 
 
 def compare_lists(int_list_one: list, int_list_two: list) -> None:
-    """Display the results showing each student's score and grade.
+    """Compare two lists and print whether they are identical.
 
-    :param scores: A list containing the scores of all students.
+    :param int_list_one: The first list of integers.
+    :param int_list_two: The second list of integers.
     :return: None
     """
-    print(f"\nThe lists are{' ' if int_list_one == int_list_two else ' not '}identical.")
+    print(
+        f"\nThe lists are{' ' if int_list_one == int_list_two else ' not '}"
+        f"identical."
+    )
 
 
 def main() -> None:
-    """Calculate statistics based on scores entered by the user.
+    """Get input for two lists of integers and compare them.
 
     :return: None
     """
