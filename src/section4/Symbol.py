71b91cadb00a0a5754a281349e13f4cff235db0c
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-06 19:21:35 -0500

    Created Symbol.py

---
+++ src/section4/Symbol.py
@@ -0,0 +1,25 @@
+def main() -> None:
+    """Construct a square pattern using a specified symbol.
+
+    :return: None
+    """
+    try:
+        # Gets the number of rows and the symbol from the user
+        rows = int(input("Enter the number of rows for the symbol square: "))
+        symbol = input("Enter the symbol to construct this square: ")
+
+        # Checks if the symbol is a single character
+        if len(symbol) != 1:
+            print("The symbol must be a single character!")
+            exit(1)
+
+        # Constructs the square pattern
+        for _ in range(rows):
+            print(symbol * rows)
+    except ValueError:
+        # Handles invalid inputs
+        print("The number of rows must be an integer! Exiting program...")
+
+
+if __name__ == "__main__":
+    main()
