ade71312b0f4ca05371c42d703bbcf5f8c379447
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-07 15:11:38 -0500

    Refactored Circle.py

--- src/section6/Circle.py
+++ src/section6/Circle.py
@@ -2,60 +2,59 @@ import math
 
 
 class Circle:
-    """Represents a rectangle with given length and width."""
+    """Represents a circle with a given radius."""
 
     def __init__(self, radius: float = 1.0) -> None:
-        """Initialize the rectangle with length and width.
+        """Initialize the circle with a radius.
 
-        :param length: The length of the rectangle (default is 1.0).
-        :param width: The width of the rectangle (default is 1.0).
+        :param radius: The radius of the circle (default is 1.0).
         :return: None
         """
         self.radius = radius
 
     def __str__(self) -> str:
-        """Return a string representation of the rectangle.
+        """Return a string representation of the circle.
 
-        :return: The string representation including length, width, perimeter,
-                 and area.
+        :return: A string containing circle information.
         """
         circumference = self.calculate_circumference()
         area = self.calculate_area()
         return (
-            f"Radius: {self.radius:.2f} unit(s)\nCircumference: "
-            f"{circumference:.2f} unit(s)\nArea: {area:.2f} "
-            f"square unit(s)"
+            f"Radius: {self.radius:.2f} unit(s)\n"
+            f"Circumference: {circumference:.2f} unit(s)\n"
+            f"Area: {area:.2f} square unit(s)"
         )
 
     def calculate_circumference(self) -> float:
-        """Calculate the perimeter of the rectangle.
+        """Calculate the circumference of the circle.
 
-        :return: The perimeter of the rectangle.
+        :return: The circumference of the circle.
         """
         return 2 * math.pi * self.radius
 
     def calculate_area(self) -> float:
-        """Calculate the area of the rectangle.
+        """Calculate the area of the circle.
 
-        :return: The area of the rectangle.
+        :return: The area of the circle.
         """
         return math.pi * self.radius ** 2
 
 
 def main() -> None:
-    """Construct and print rectangles with default and custom dimensions.
+    """Create and display information for a circle with the default and modified
+    radius.
 
     :return: None
     """
-    # Constructs and prints the default circle
+    # Creates and prints the default circle
     print("Default circle:")
-    circle = Circle()
-    print(circle)
+    default_circle = Circle()
+    print(default_circle)
 
-    # Sets the radius to 10 and prints the modified circle
+    # Modifies the radius and prints the modified circle
     print("\nModified default circle:")
-    circle.radius = 10
-    print(circle)
+    default_circle.radius = 10
+    print(default_circle)
 
 
 if __name__ == "__main__":
