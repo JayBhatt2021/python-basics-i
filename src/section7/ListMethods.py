ec7a42d2e388ff4d51f853a6fcf029bedaf17a8b
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-07 18:38:47 -0500

    Refactored ListMethods.py

--- src/section7/ListMethods.py
+++ src/section7/ListMethods.py
@@ -1,36 +1,41 @@
 import random
 
 
-def generate_random_list() -> list:
-    """Prompt the user to enter integers for each list.
+def generate_random_list(size: int = 5, start: int = 1, end: int = 100) -> list:
+    """Generate a list of random integers within a specified range.
 
-    :return: A list containing the integers entered by the user.
+    :param size: The size of the list (default is 5).
+    :param start: The lower bound of the random range (default is 1).
+    :param end: The upper bound of the random range (default is 100).
+    :return: A list containing random integers.
     """
-    return [random.randint(1, 100) for _ in range(5)]
+    return [random.randint(start, end) for _ in range(size)]
 
 
 def square_list(input_list: list) -> list:
-    """Prompt the user to enter integers for each list.
+    """Square each element of the input list.
 
-    :return: A list containing the integers entered by the user.
+    :param input_list: The list to be squared.
+    :return: A list containing the squared elements.
     """
     return [element ** 2 for element in input_list]
 
 
 def main() -> None:
-    """Generate and display a list of random numbers within a specified range.
+    """Generate a random list, perform operations, and display results.
 
     :return: None
     """
     random_list = generate_random_list()
-
     print("Original List:", random_list)
     print("Max Value:", max(random_list))
     print("Min Value:", min(random_list))
-    print("Squared List:", square_list(random_list))
 
-    random_list.reverse()
-    print("Reversed List:", random_list)
+    squared_list = square_list(random_list)
+    print("Squared List:", squared_list)
+
+    reversed_squared_list = list(reversed(squared_list))
+    print("Reversed Squared List:", reversed_squared_list)
 
 
 if __name__ == "__main__":
