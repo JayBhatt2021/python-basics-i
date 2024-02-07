5d1bcfe50c7cf6e5a04bbe5a936f4782633b5f12
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-07 12:34:58 -0500

    Refactored RectangleClass.py

--- src/section6/RectangleClass.py
+++ src/section6/RectangleClass.py
@@ -1,45 +1,46 @@
 class Rectangle:
-    """Represents a quadratic equation of the form ax^2 + bx + c = 0."""
+    """Represents a rectangle with given length and width."""
 
     def __init__(self, length: float = 1.0, width: float = 1.0) -> None:
-        """Initialize the quadratic equation with coefficients.
+        """Initialize the rectangle with length and width.
 
-        :param a: Coefficient of the quadratic term (ax^2).
-        :param b: Coefficient of the linear term (bx).
-        :param c: Constant term in the quadratic equation.
+        :param length: The length of the rectangle (default is 1.0).
+        :param width: The width of the rectangle (default is 1.0).
         :return: None
         """
         self.length = length
         self.width = width
 
     def __str__(self) -> str:
-        """
+        """Return a string representation of the rectangle.
 
-        :return:
+        :return: The string representation including length, width, perimeter,
+                 and area.
         """
+        perimeter = self.calculate_perimeter()
+        area = self.calculate_area()
         return (
             f"Length: {self.length:.2f} units\nWidth: {self.width:.2f} units\n"
-            f"Perimeter: {self.calculate_perimeter()} units\nArea: "
-            f"{self.calculate_area()} square units"
+            f"Perimeter: {perimeter:.2f} units\nArea: {area:.2f} square units"
         )
 
     def calculate_perimeter(self) -> float:
-        """Calculate the roots of the quadratic equation.
+        """Calculate the perimeter of the rectangle.
 
-        :return: A tuple containing the roots of the quadratic equation.
+        :return: The perimeter of the rectangle.
         """
         return 2 * (self.length + self.width)
 
     def calculate_area(self) -> float:
-        """Calculate and return the discriminant of the quadratic equation.
+        """Calculate the area of the rectangle.
 
-        :return: The discriminant value.
+        :return: The area of the rectangle.
         """
         return self.length * self.width
 
 
 def main() -> None:
-    """Calculate the roots of a quadratic equation.
+    """Construct and print rectangles with default and custom dimensions.
 
     :return: None
     """
@@ -48,10 +49,10 @@ def main() -> None:
     default_rectangle = Rectangle()
     print(default_rectangle)
 
-    # Constructs and prints an overloaded rectangle
-    print("\nOverloaded rectangle:")
-    overloaded_rectangle = Rectangle(5, 6)
-    print(overloaded_rectangle)
+    # Constructs and prints a custom rectangle
+    print("\nCustom rectangle:")
+    custom_rectangle = Rectangle(5, 6)
+    print(custom_rectangle)
 
 
 if __name__ == "__main__":
