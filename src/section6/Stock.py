c3a0b64131f87ffec2d48a10d122c91f45be69a4
Jay Bhatt <jay.bhatt98@gmail.com>
2024-02-07 14:53:16 -0500

    Refactored Stock.py

--- src/section6/Stock.py
+++ src/section6/Stock.py
@@ -1,12 +1,11 @@
 class Stock:
-    """Represents a rectangle with given length and width."""
+    """Represents a stock with a name, symbol, and price information."""
 
     def __init__(self, name: str, symbol: str) -> None:
-        """Initialize the rectangle with length and width.
+        """Initialize a stock with a name and symbol.
 
-        :param length: The length of the rectangle (default is 1.0).
-        :param width: The width of the rectangle (default is 1.0).
-        :return: None
+        :param name: The name of the stock.
+        :param symbol: The symbol of the stock.
         """
         self.name = name
         self.symbol = symbol
@@ -14,44 +13,41 @@ class Stock:
         self.current_price = 0.0
 
     def __str__(self) -> str:
-        """Return a string representation of the rectangle.
+        """Return a string representation of the stock.
 
-        :return: The string representation including length, width, perimeter,
-                 and area.
+        :return: A string containing stock information.
         """
         change_percent = self.calculate_change_percent()
         return (
-            f"{self.name} stock\nSymbol: {self.symbol}\nClosing price: "
-            f"${self.previous_closing_price:.2f}\nCurrent price: $"
-            f"{self.current_price:.2f}\nChange percent: {change_percent:.2f}%"
+            f"{self.name} Stock\nSymbol: {self.symbol}\nClosing Price: "
+            f"${self.previous_closing_price:.2f}\nCurrent Price: $"
+            f"{self.current_price:.2f}\nChange Percent: {change_percent:.2f}%"
         )
 
     def calculate_change_percent(self) -> float:
-        """Calculate the perimeter of the rectangle.
+        """Calculate the percentage change between previous and current prices.
 
-        :return: The perimeter of the rectangle.
+        :return: The percentage change in price.
         """
-        return (
-                self.current_price - self.previous_closing_price) / self.current_price * 100
+        return ((self.current_price - self.previous_closing_price) /
+                self.previous_closing_price * 100)
 
 
 def main() -> None:
-    """Construct and print rectangles with default and custom dimensions.
+    """Create and display information for Google and Microsoft stocks.
 
     :return: None
     """
-    # Constructs and prints the Google stock
-    google_stock = Stock("Google", "GOG")
+    # Google stock information
+    google_stock = Stock("Google", "GOOG")
     google_stock.previous_closing_price = 134.67
     google_stock.current_price = 131.98
-
     print(f"{google_stock}\n")
 
-    # Constructs and prints the Microsoft stock
-    microsoft_stock = Stock("Microsoft", "MSF")
+    # Microsoft stock information
+    microsoft_stock = Stock("Microsoft", "MSFT")
     microsoft_stock.previous_closing_price = 156.52
     microsoft_stock.current_price = 161.22
-
     print(microsoft_stock)
 
 
